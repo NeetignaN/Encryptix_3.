9a2bbd8f53d388ab8ba67eb3c002ff50b9aa36aa
NeetignaN <nagulaneetigna@gmail.com>
2024-07-02 23:44:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function updateDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    var displayValue = document.getElementById('display').value;
+    document.getElementById('display').value = displayValue.slice(0, -1);
+}
+
+function calculate() {
+    var displayValue = document.getElementById('display').value;
+    try {
+        var result = eval(displayValue);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
